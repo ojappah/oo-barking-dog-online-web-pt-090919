dc74f44c97117f90326d96fbedcc798db8376dc5
generous-vector-9581 <ojappah@gmail.com>
2019-11-03 03:54:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+<<<<<<< HEAD
   def name=(dogs_name)
     @name = dogs_name
 end
@@ -7,5 +8,11 @@ def name
 end
 def bark
   puts "woof!"
+=======
+  def .name=(name)
+end
+def .name 
+  .name = name
+>>>>>>> d84b715c3f9dea3a247f514e32d60e3d9e60a2bf
 end
 end
